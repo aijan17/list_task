d80be5ed57aee82aedee0a0b7185c6bdc602823e
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-02-25 15:34:33 +0600

    added model Task for tables

--- list_task/webapp/models.py
+++ list_task/webapp/models.py
@@ -1,3 +1,23 @@
+
 from django.db import models
 
-# Create your models here.
+status_choices = (
+    ('new', 'Новая'),
+    ('in_progress', 'В процессе'),
+    ('done', 'Сделано')
+)
+
+
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=200, null=False, blank=False)
+    status = models.CharField(max_length=100, default='new',  choices=status_choices, blank=False)
+    pub_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return '{}: {}'.format(self.id, self.description, self.status)
+
+    class Meta:
+        verbose_name = 'задача'
+        verbose_name_plural = "задачи"
+
