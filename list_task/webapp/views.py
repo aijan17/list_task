87e70a5591730492a1b76daef0d12a3e9d5a6de2
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-02-25 18:39:08 +0600

    functions for views , list of task,one task viewing,create task

--- list_task/webapp/views.py
+++ list_task/webapp/views.py
@@ -1,30 +1,39 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from webapp.models import Task
+from webapp.models import Task, status_choices
 
 
 def list_of_task_view(request):
-    list_tasks = Task.objects.order_by('pub_date')
+    list_tasks = Task.objects.all()
     return render(request, 'list_of_task_view.html', {"list_tasks": list_tasks})
 
 
 def task_view(request):
     task_id = request.GET.get('id')
-    task = Task.objects.get(description=task_id)
+    task = Task.objects.get(id=task_id)
     return render(request, 'task_view.html', {'task': task})
 
 
 def task_create_view(request):
     if request.method == "GET":
-        return render(request, 'task_create_view.html')
+        return render(request, 'task_create_view.html', {'status': status_choices})
     elif request.method == "POST":
         description = request.POST.get("description")
         status = request.POST.get("status")
         pub_date = request.POST.get("pub_date")
+        if pub_date == '':
+            pub_date = None
 
         task = Task.objects.create(
             description=description,
             status=status,
             pub_date=pub_date,
         )
+        return render(request, 'task_view.html', {'task': task})
 
-        return render(request, 'task_create_view.html', {'task': task})
+
+def remove_view(request):
+    id = request.GET.get('id')
+    task_remove = Task.objects.get(id=id)
+    task_remove.delete()
+    return HttpResponseRedirect('/')
