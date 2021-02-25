dd7e34c7853e25471755b4b471165fb80faf5647
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-02-25 18:40:00 +0600

    registration in the admin panel

--- list_task/webapp/admin.py
+++ list_task/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import Task
 
-# Register your models here.
+admin.site.register(Task)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'pub_date']
+    list_filter = ['description']
+    search_fields = ['description', 'status']
+    fields = ['description', 'status', 'pub_date']
+    readonly_fields = ['pub_date']
