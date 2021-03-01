66c4dedbe75c762ed1bd2c8badff1e28fa93dec1
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-03-01 17:53:26 +0600

    Added a field for detailed description to the problem model

--- list_task/webapp/models.py
+++ list_task/webapp/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 status_choices = (
@@ -11,13 +10,13 @@ status_choices = (
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=200, null=False, blank=False)
-    status = models.CharField(max_length=100, default='new',  choices=status_choices, blank=False)
+    text_for_detailed = models.TextField(max_length=300, blank=True, null=True)
+    status = models.CharField(max_length=100, default='new', choices=status_choices, blank=False)
     pub_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
-        return '{}: {}'.format(self.id, self.description, self.status)
+        return '{}: {}'.format(self.id, self.description, self.text_for_detailed, self.status)
 
     class Meta:
         verbose_name = 'задача'
         verbose_name_plural = "задачи"
-
