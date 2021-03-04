5c13083373ed845bf2dcd1749ec7c1db02170db4
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-03-04 18:07:06 +0600

    updated

--- list_task/list_task/urls.py
+++ list_task/list_task/urls.py
@@ -13,16 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from webapp.views import task_view, list_of_task_view, task_create_view, remove_view
+from webapp.views import task_view, \
+    list_of_task_view, \
+    task_create_view, \
+    remove_view, \
+    task_update_view
 
+HOMEPAGE_URL = 'task/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task/<int:id>',task_view,  name='task-view'),
+    path('task/<int:id>', task_view,  name='task-view'),
     path('', list_of_task_view, name='list-view'),
     path('add/', task_create_view, name='task-add'),
     path('remove/<int:id>', remove_view, name='delete-task'),
+    path('update/<int:id>', task_update_view, name='task-update')
+
+    ,
 
 ]
+
